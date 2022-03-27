9eff8ebe6ad0844f714b32c7f7382c102f0792fa
rhodeon <ruonabraks@gmail.com>
2022-03-28 00:52:18 +0100

    Implemented parsing of configs from environment variables.

--- cmd/api/common/config.go
+++ cmd/api/common/config.go
@@ -1,6 +1,10 @@
 package common
 
-import "flag"
+import (
+	"flag"
+	"os"
+	"strconv"
+)
 
 type Config struct {
 	Env     string
@@ -9,9 +13,39 @@ type Config struct {
 }
 
 func (c *Config) Parse() {
-	flag.StringVar(&c.Env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&c.Version, "version", "1.0.0", "API version")
-	flag.IntVar(&c.Port, "port", 8080, "API server port")
+	flag.StringVar(&c.Env, "env", c.defaultEnv(), "Environment (development|staging|production)")
+	flag.StringVar(&c.Version, "version", c.defaultVersion(), "API version")
+	flag.IntVar(&c.Port, "port", c.defaultPort(), "API server port")
 
 	flag.Parse()
+}
+
+func (c *Config) defaultEnv() string {
+	const defaultEnv = "development"
+
+	if env, exists := os.LookupEnv("ENV"); exists {
+		return env
+	}
+	return defaultEnv
+}
+
+func (c *Config) defaultVersion() string {
+	const defaultVer = "1.0.0"
+
+	if version, exists := os.LookupEnv("VERSION"); exists {
+		return version
+	}
+	return defaultVer
+}
+
+func (c *Config) defaultPort() int {
+	const defaultPort = 8080
+
+	if portEnv, exists := os.LookupEnv("PORT"); exists {
+		port, err := strconv.Atoi(portEnv)
+		if err == nil {
+			return port
+		}
+	}
+	return defaultPort
 }
