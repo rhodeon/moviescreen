56c52c3a3813b85742e3536b0cca09883e78f2c2
rhodeon <ruonabraks@gmail.com>
2022-04-05 03:13:14 +0100

    Implemented tests for movie list handler function.

---
+++ infrastructure/mock/helpers.go
@@ -0,0 +1,79 @@
+package mock
+
+import (
+	"github.com/rhodeon/moviescreen/domain/models"
+	"sort"
+	"strings"
+)
+
+// caseInsensitiveSubslice checks if the target slice contains the data slice.
+func caseInsensitiveSubslice(data []string, target []string) bool {
+	if len(data) > len(target) {
+		return false
+	}
+	for _, element := range data {
+		if !caseInsensitiveIn(element, target) {
+			return false
+		}
+	}
+	return true
+}
+
+// In returns true if the data is found in the target list.
+// Both values are converted to lowercase for comparison.
+func caseInsensitiveIn(data string, target []string) bool {
+	for _, element := range target {
+		if strings.ToLower(element) == strings.ToLower(data) {
+			return true
+		}
+	}
+	return false
+}
+
+func sortMoviesById(movies models.Movies, ascending bool) {
+	if ascending {
+		sort.Slice(movies, func(i, j int) bool {
+			return movies[i].Id < movies[j].Id
+		})
+	} else {
+		sort.Slice(movies, func(i, j int) bool {
+			return movies[i].Id > movies[j].Id
+		})
+	}
+}
+
+func sortMoviesByTitle(movies models.Movies, ascending bool) {
+	if ascending {
+		sort.Slice(movies, func(i, j int) bool {
+			return movies[i].Title < movies[j].Title
+		})
+	} else {
+		sort.Slice(movies, func(i, j int) bool {
+			return movies[i].Title > movies[j].Title
+		})
+	}
+}
+
+func sortMoviesByYear(movies models.Movies, ascending bool) {
+	if ascending {
+		sort.Slice(movies, func(i, j int) bool {
+			return movies[i].Year < movies[j].Year
+		})
+	} else {
+		sort.Slice(movies, func(i, j int) bool {
+			return movies[i].Year > movies[j].Year
+		})
+	}
+}
+
+func sortMoviesByRuntime(movies models.Movies, ascending bool) {
+	if ascending {
+		sort.Slice(movies, func(i, j int) bool {
+			return movies[i].Runtime < movies[j].Runtime
+		})
+	} else {
+		sort.Slice(movies, func(i, j int) bool {
+			return movies[i].Runtime > movies[j].Runtime
+		})
+	}
+}
--- infrastructure/mock/movieController.go
+++ infrastructure/mock/movieController.go
@@ -1,8 +1,11 @@
 package mock
 
 import (
+	"github.com/rhodeon/moviescreen/cmd/api/models/request"
+	"github.com/rhodeon/moviescreen/cmd/api/models/response"
 	"github.com/rhodeon/moviescreen/domain/models"
 	"github.com/rhodeon/moviescreen/domain/repository"
+	"strings"
 	"time"
 )
 
@@ -16,7 +19,6 @@ var movies = []models.Movie{
 		Version: 1,
 		Created: time.Now(),
 	},
-
 	{
 		Id:      2,
 		Title:   "Hamilton",
@@ -45,6 +47,54 @@ func (m MovieController) Get(id int) (models.Movie, error) {
 	}
 
 	return models.Movie{}, repository.ErrRecordNotFound
+}
+
+func (m MovieController) List(title string, genres []string, filters request.Filters) (models.Movies, response.Metadata, error) {
+	movieList := models.Movies{}
+
+	// add movies which match both the title and the genres
+	for _, movie := range movies {
+		if strings.Contains(movie.Title, title) && caseInsensitiveSubslice(genres, movie.Genres) {
+			movieList = append(movieList, movie)
+		}
+	}
+
+	// determine ending index based on page limit
+	stop := filters.Offset() + filters.Limit
+	if stop > len(movieList) {
+		stop = len(movieList)
+	}
+
+	// get the total number of found records before reassigning
+	// the movie list based on the page number and limit
+	totalRecords := len(movieList)
+	metadata := response.CalculateMetadata(filters.Page, filters.Limit, totalRecords)
+	movieList = movieList[filters.Offset():stop]
+
+	// sort based on the filter
+	switch filters.Sort {
+	case "id":
+		sortMoviesById(movieList, true)
+	case "-id":
+		sortMoviesById(movieList, false)
+
+	case "title":
+		sortMoviesByTitle(movieList, true)
+	case "-title":
+		sortMoviesByTitle(movieList, false)
+
+	case "year":
+		sortMoviesByYear(movieList, true)
+	case "-year":
+		sortMoviesByYear(movieList, false)
+
+	case "runtime":
+		sortMoviesByRuntime(movieList, true)
+	case "-runtime":
+		sortMoviesByRuntime(movieList, false)
+	}
+
+	return movieList, metadata, nil
 }
 
 func (m MovieController) Update(movie *models.Movie) error {
