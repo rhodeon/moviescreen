f12754402169e8775f23cbf36e04f3380d5ec38f
rhodeon <ruonabraks@gmail.com>
2022-04-25 05:31:22 +0100

    Added test for deleting tokens.

--- infrastructure/database/tokenController_test.go
+++ infrastructure/database/tokenController_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"database/sql"
 	"github.com/rhodeon/moviescreen/domain/models"
 	"github.com/rhodeon/moviescreen/infrastructure/mock"
 	"github.com/rhodeon/moviescreen/internal/testhelpers"
@@ -31,7 +32,9 @@ func TestTokenController_Insert(t *testing.T) {
 	testhelpers.AssertError(t, err, nil)
 
 	// query database to confirm the token was successfully inserted
-	stmt := `SELECT hash, user_id, scope, expires FROM tokens where user_id = $1 AND scope = $2`
+	stmt := `SELECT hash, user_id, scope, expires FROM tokens 
+	where user_id = $1 AND scope = $2`
+
 	fetchedToken := models.Token{}
 	row := db.QueryRow(stmt, token.UserId, token.Scope)
 
@@ -46,5 +49,32 @@ func TestTokenController_Insert(t *testing.T) {
 	// compare the original and fetched tokens
 	if !reflect.DeepEqual(fetchedToken, token) {
 		t.Errorf("\nGot:\t%+v\nWant:\t%+v", fetchedToken, token)
+	}
+}
+
+func TestTokenController_DeleteAllForUser(t *testing.T) {
+	if testing.Short() {
+		t.Skip("postgres: skipping integration test")
+	}
+
+	db, teardown := newTestDb(t)
+	tokenController := TokenController{Db: db}
+	defer teardown()
+
+	userId := 1
+	err := tokenController.DeleteAllForUser(userId, models.ScopeActivation)
+
+	testhelpers.AssertError(t, err, nil)
+
+	// query tokens to ensure all qualified ones were deleted
+	stmt := `SELECT hash, user_id, scope, expires FROM tokens
+	WHERE (user_id = $1 AND scope = $2) OR expires < now()`
+
+	fetchedToken := models.Token{}
+	row := tokenController.Db.QueryRow(stmt, userId, models.ScopeActivation)
+
+	err = row.Scan(&fetchedToken.Hash, &fetchedToken.UserId, &fetchedToken.Scope, &fetchedToken.Expires)
+	if err != sql.ErrNoRows {
+		t.Errorf("\nGot:\t%+v\nWant: an empty token", fetchedToken)
 	}
 }
